7a74e77873ada581bdad3f59e17b5513af67b35f
Oz Blech <ozblech87@gmail.com>
2025-08-18 10:43:53 +0300

    fix script to use source

--- infrastructure/main/deploy-and-connect.sh
+++ infrastructure/main/deploy-and-connect.sh
@@ -10,6 +10,6 @@ echo "✅ Terraform apply completed."
 sleep 5
 
 echo "🔗 Running connection script: ./connect-to-minikube.sh..."
-./connect-to-minikube.sh
+source ./connect-to-minikube.sh
 
 echo "🎉 All done!"
